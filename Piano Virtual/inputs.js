fd38b9689b3b2e2d895852a95ae419352bd19e31
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-06-28 02:54:49 -0300

    consertei o bug de tecla nao se repetindo

--- Piano Virtual/inputs.js
+++ Piano Virtual/inputs.js
@@ -4,10 +4,10 @@ let swith = false
 
 let pianoKeys = {
     la3 : document.querySelector('[name="la3"]').addEventListener("click", function(){
-        sounds.si3.pause()
     }),
     si3 : document.querySelector('[name="si3"]').addEventListener("click", function(){
         sounds.si3.play()
+        sounds.si3.currentTime = 0
     })
 }
 
