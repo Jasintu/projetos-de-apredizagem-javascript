5345a6d728ec8281e9852d7a3155458fae1e1993
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-06-29 03:02:30 -0300

    adicionei todas id e names nas teclas

--- Piano Virtual/inputs.js
+++ Piano Virtual/inputs.js
@@ -1,18 +1,24 @@
 "use strict"
 
-let swith = false
-
 let pianoKeys = {
-    la3 : document.querySelector('[name="la3"]').addEventListener("click", function(){
+    fa3 : $("[name='fa3']").click(()=>{
+        let audio = $("#fa3")[0]
+        audio.play()
+        audio.currentTime = 0
     }),
-    si3 : document.querySelector('[name="si3"]').addEventListener("click", function(){
-        sounds.si3.play()
-        sounds.si3.currentTime = 0
-    })
+    sol3 : $("[name='sol3']").click(()=>{
+        let audio = $("#sol3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    la3 : $("[name='la3']").click(()=>{
+        let audio = $("#la3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    si3 : $("[name='si3']").click(()=>{
+        let audio = $("#si3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
 }
-
-let sounds = {
-    si3 : document.getElementById("si3")
-}
-
-
