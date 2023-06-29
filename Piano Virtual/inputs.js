d870a88d7b03127d944688f7eaa656e0c28f0227
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-06-29 03:09:01 -0300

    todas as teclas brancas agora funcionam

--- Piano Virtual/inputs.js
+++ Piano Virtual/inputs.js
@@ -1,6 +1,91 @@
 "use strict"
 
 let pianoKeys = {
+    do1 : $("[name='do1']").click(()=>{
+        let audio = $("#do1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    re1 : $("[name='re1']").click(()=>{
+        let audio = $("#re1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    mi1 : $("[name='mi1']").click(()=>{
+        let audio = $("#mi1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    fa1 : $("[name='fa1']").click(()=>{
+        let audio = $("#fa1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    sol1 : $("[name='sol1']").click(()=>{
+        let audio = $("#sol1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    la1 : $("[name='la1']").click(()=>{
+        let audio = $("#la1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    si1 : $("[name='si1']").click(()=>{
+        let audio = $("#si1")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    do2 : $("[name='do2']").click(()=>{
+        let audio = $("#do2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    re2 : $("[name='re2']").click(()=>{
+        let audio = $("#re2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    mi2 : $("[name='mi2']").click(()=>{
+        let audio = $("#mi2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    fa2 : $("[name='fa2']").click(()=>{
+        let audio = $("#fa2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    sol2 : $("[name='sol2']").click(()=>{
+        let audio = $("#sol2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    la2 : $("[name='la2']").click(()=>{
+        let audio = $("#la2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    si2 : $("[name='si2']").click(()=>{
+        let audio = $("#si2")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    do3 : $("[name='do3']").click(()=>{
+        let audio = $("#do3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    re3 : $("[name='re3']").click(()=>{
+        let audio = $("#re3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
+    mi3 : $("[name='mi3']").click(()=>{
+        let audio = $("#mi3")[0]
+        audio.play()
+        audio.currentTime = 0
+    }),
     fa3 : $("[name='fa3']").click(()=>{
         let audio = $("#fa3")[0]
         audio.play()
