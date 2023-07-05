7dc02599724913a63ada5ee2d8e74cf822dc0598
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-07-05 02:06:31 -0300

    adicionei vitoria pra jogador

--- STATLLEʙʀ/scriptjs/hiddenstates.js
+++ STATLLEʙʀ/scriptjs/hiddenstates.js
@@ -1,33 +1,33 @@
 "use strict"
 
 const states = {
-    1: 'Acre',
-    2: 'Alagoas',
-    3: 'Amapá',
-    4: 'Amazonas',
-    5: 'Bahia',
-    6: 'Ceará',
-    7: 'Distrito Federal',
-    8: 'Espírito Santo',
-    9: 'Goiás',
-    10: 'Maranhão',
-    11: 'Mato Grosso',
-    12: 'Mato Grosso do Sul',
-    13: 'Minas Gerais',
-    14: 'Pará',
-    15: 'Paraíba',
-    16: 'Paraná',
-    17: 'Pernambuco',
-    18: 'Piauí',
-    19: 'Rio de Janeiro',
-    20: 'Rio Grande do Norte',
-    21: 'Rio Grande do Sul',
-    22: 'Rondônia',
-    23: 'Roraima',
-    24: 'Santa Catarina',
-    25: 'São Paulo',
-    26: 'Sergipe',
-    27: 'Tocantins'
+    0: 'ACRE',
+    1: 'ALAGOAS',
+    2: 'AMAPÁ',
+    3: 'AMAZONAS',
+    4: 'BAHIA',
+    5: 'CEARÁ',
+    6: 'DISTRITO FEDERAL',
+    7: 'ESPÍRITO SANTO',
+    8: 'GOIÁS',
+    9: 'MARANHÃO',
+    10: 'MATO GROSSO',
+    11: 'MATO GROSSO DO SUL',
+    12: 'MINAS GERAIS',
+    13: 'PARÁ',
+    14: 'PARAÍBA',
+    15: 'PARANÁ',
+    16: 'PERNAMBUCO',
+    17: 'PIAUÍ',
+    18: 'RIO GRANDE DO NORTE',
+    19: 'RIO DE JANEIRO',
+    20: 'RIO GRANDE DO SUL',
+    21: 'RONDÔNIA',
+    22: 'RORAIMA',
+    23: 'SANTA CATARINA',
+    24: 'SÃO PAULO',
+    25: 'SERGIPE',
+    26: 'TOCANTINS'
 }
 
 const statsterritory = {
@@ -49,9 +49,9 @@ const statsterritory = {
     15: "territorios/Paraná.png",
     16: "territorios/Pernambuco.png",
     17: "territorios/Piauí.png",
-    18: "territorios/RioJaneiro.png",
-    19: "territorios/RioGrandeNorte.png",
-    20: "territorios/RioGrandeSul.png",
+    18: "territorios/RioGrandeNorte.png",
+    19: "territorios/RioGrandeSul.png",
+    20: "territorios/RioJaneiro.png",
     21: "territorios/Rondônia.png",
     22: "territorios/Roraima.png",
     23: "territorios/SantaCatarina.png",
@@ -64,3 +64,18 @@ let NumberRandom = Math.round(Math.random() * 26)
 
 $("#localimage").append(`<img src="${statsterritory[NumberRandom]}" alt="" id="imgstate"></img>`)
 
+/*dar resultado de vitória*/
+
+let result = String(states[NumberRandom])
+
+$("#buttonSubmti").click(function(){
+    if($("#stateInput").val() == result){
+    console.log($("#stateInput").val())
+
+    }
+})
+
+console.log(states[NumberRandom])
+console.log(NumberRandom)
+console.log(result)
+console.log(typeof result)
